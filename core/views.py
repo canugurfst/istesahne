c742a753a7577c411a46ce15fcdb478db48ff352
Can Ugur <canugurfst@gmail.com>
2026-06-23 20:55:26 +0300

    Complete CMS pages and homepage videos

--- core/views.py
+++ core/views.py
@@ -1,10 +1,25 @@
+from core.models import PageContent
 from django.shortcuts import render
+from videos.models import Video
 
 def home(request):
-    return render(request, 'core/home.html')
+    page = PageContent.objects.get(page="home")
+
+    latest_videos = Video.objects.order_by('-created_at')[:3]
+
+    return render(
+        request, 
+        'core/home.html',
+        {
+            'page': page,
+            'latest_videos': latest_videos,
+        }
+    )
 
 def about(request):
-    return render(request, 'core/about.html')
+    page = PageContent.objects.get(page="about")
+    return render(request, "core/about.html", {"page": page})
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    page = PageContent.objects.get(page="contact")
+    return render(request, "core/contact.html", {"page": page})
